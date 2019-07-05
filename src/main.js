a506800957a10c4f16a8ad88edace5bf1136f320
Nick Lewis <nickeblewis@gmail.com>
2019-07-05 14:55:52 +0100

    fixed metadata

--- src/main.js
+++ src/main.js
@@ -18,17 +18,17 @@ export default function (Vue, { router, head, isClient }) {
 
   head.meta.push({
     name: 'keywords',
-    content: 'Gridsome,Vue,Tailwind,Tailwind CSS,JavaScript,HTML,CSS,Vue.js,VueJS'
+    content: 'Bournemouth,Jazz,Big Band,Music,Club'
   })
 
   head.meta.push({
     name: 'description',
-    content: 'Gridsome Portfolio Starter'
+    content: 'Bournemouth Swing and Big Band Jazz Club'
   })
 
   head.meta.push({
     name: 'author',
-    content: 'Andre Madarang'
+    content: 'Nick Lewis'
   })
 
   head.link.push({
