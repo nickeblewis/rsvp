0c5b94c134c461130c99a05d813dbca4d70abd11
Nick Lewis <nickeblewis@gmail.com>
2019-09-04 20:55:12 +0100

    First commit

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -1,47 +1,6 @@
 <template>
   <div class="content-wrapper bg-background-primary font-sans text-copy-primary leading-normal flex flex-col min-h-screen" :class="theme">
-    <header class="border-t-14 border-orange-700">
-      <nav class="container mx-auto flex flex-wrap justify-between items-center py-8">
-        <div>
-          <g-link v-if="theme === 'theme-light'" to="/"></g-link>
-          <g-link v-else to="/"></g-link>
-        </div>
-        <div class="block lg:hidden">
-          <button @click="toggle" class="flex items-center px-3 py-2 border rounded border-gray-500 hover:text-gray-600 hover:border-gray-600">
-            <svg class="current-color h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" fill="gray" /></svg>
-          </button>
-        </div>
-        <ul
-          class="uppercase tracking-wide font-bold w-full block flex-grow lg:flex lg:flex-initial lg:w-auto items-center mt-8 lg:mt-0"
-          :class="isOpen ? 'block': 'hidden'"
-        >
-          <li class="mr-8 mb-6 lg:mb-0">
-            <search-input />
-          </li>
-          <li class="mr-8 mb-6 lg:mb-0">
-            <theme-switcher :theme="theme" @themeChanged="updateTheme" />
-          </li>
-          <li class="mr-8 mb-6 lg:mb-0">
-            <g-link to="/" class="text-copy-primary hover:text-gray-600">Home</g-link>
-          </li>
-          <li class="mr-8 mb-6 lg:mb-0">
-            <a v-if="$route.path === '/'" href="/#events" v-scroll-to="'#events'" class="text-copy-primary hover:text-gray-600">Events</a>
-            <g-link v-else to="/#events" class="text-copy-primary hover:text-gray-600">Events</g-link>
-          </li>
-          <li class="mr-8 mb-6 lg:mb-0">
-            <a v-if="$route.path === '/'" href="/#about" v-scroll-to="'#about'" class="text-copy-primary hover:text-gray-600">About</a>
-            <g-link v-else to="/#about" class="text-copy-primary hover:text-gray-600">About</g-link>
-          </li>
-          <li class="mr-8 mb-6 lg:mb-0">
-            <a v-if="$route.path === '/'" href="/#contact" v-scroll-to="'#contact'" class="text-copy-primary hover:text-gray-600">Contact</a>
-            <g-link v-else to="/#contact" class="text-copy-primary hover:text-gray-600">Contact</g-link>
-          </li>
-          <li>
-            <g-link to="/blog" class="text-copy-primary hover:text-gray-600">Blog</g-link>
-          </li>
-        </ul>
-      </nav>
-    </header>
+   
 
     <div class="flex-grow">
       <slot/>
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -2,131 +2,35 @@
   <Layout>
     <div class="hero container-inner mx-auto flex flex-col sm:flex-row justify-between py-16">
       <div class="text-4xl font-bold w-full sm:w-3/5 text-center sm:text-left">
-        <div class="leading-tight">Welcome to the</div>
-        <div class="text-orange-700 leading-tight">Bournemouth Big Band Swing and Jazz Club</div>
+        <div class="leading-tight">You are invited to attend</div>
+        <div class="text-orange-700 leading-tight">The Celebration of Alice and Matt Allen's Marriage</div>
       </div>
-      <div class="mt-8 sm:mt-0">
-        <g-image src="../../static/jazzlogo.png" alt="hero" class="mx-auto sm:mx-0" />
-      </div>
+
     </div> <!-- end hero -->
 
     <div class="container-inner mx-auto" id="about">
-      <p class="text-lg sm:text-xl mb-4">The Bournemouth Big Band Swing and Jazz Club meets on the second Wednesday of each month (7.15 - 9.45pm) in the Sharp Room of the <a href="https://kinsoncommunitycentre.org.uk/" target="_blank">Kinson Community Centre</a> Bournemouth.</p>
+      <p class="text-lg sm:text-xl mb-4">Please RSVP etc etc</p>
       
-      <p class="text-lg sm:text-xl mb-4">Currently we have around 25 active members (Ladies and Gents) who all share a common interest in recorded Jazz music of all kinds. This can (and frequently does) range from early Dixieland to Modern Jazz via Mainstream and Big Band.</p>
+      <p class="text-lg sm:text-xl mb-4">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Excepturi optio odit mollitia illo ullam rem qui, nulla sit vero asperiores magni, quidem, impedit dolores debitis dignissimos molestiae commodi dicta eligendi!</p>
 
-      <p class="text-lg sm:text-xl mb-4">The normal format of each meeting is simple - 3 members will each present 40 programme minutes of recorded Jazz music of their own choice - usually with a clever or interesting theme.</p>
+      <p class="text-lg sm:text-xl mb-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus voluptates quam aliquam atque voluptatum, impedit, temporibus consequatur, quae omnis earum autem provident nisi explicabo eveniet neque quod numquam voluptate illo.</p>
         
-      <p class="text-lg sm:text-xl mb-4">We have state of the art equipment so the sound quality is superb. After the first two presentations there is a break of around 20 minutes for tea/coffee and biscuits and a chance to socialise. Occasionally we have a guest speaker.</p>
 
-      <p class="text-lg sm:text-xl mb-4">New members are most welcome so why not come along and join us, your first evening will be totally free of charge. If you like what you hear and see then we would love you to join us as a permanent member.</p>
-        
-      <p class="text-lg sm:text-xl mb-4">There is a one-off annual cost of £4 which makes you a member of the Kinson CC and the entry fee for each meeting is just £2 which covers the cost of your refreshment as well as a fantastic evening of recorded jazz music.</p>
-
-      <p class="text-lg sm:text-xl mb-4">After each Meeting we send out a newsletter via email to all members with album and personnel details about all the music played.</p>
-
-      <p class="text-lg sm:text-xl mb-4">A really great way to meet folk who all love jazz music.</p>
 
     
     </div>
 
-    <div class="overflow-x-hidden">
-      <div class="projects container-inner mx-auto text-xl border-t border-gray-500 border-b py-16 mb-16 relative">
-        <h2 class="font-bold mb-6" id="events">Forthcoming meetings and topics:</h2>
-
-        <div class="absolute right-0" style="top: 50px; transform: translate(100%) rotate(180deg)">
-          <svg width="170px" height="170px"><use xlink:href="#dots-triangle" /></svg>
-        </div>
-
-        <ul class="text-lg sm:text-xl">
-          <li class="checkmark mb-6">
-            <div>WEDNESDAY THE 10th JULY 7.15pm</div>
-            <div class="text-lg text-gray-600">“You're On!” Each member presents a single track from a favourite Jazz CD. </div>
-          </li>
-          <li class="checkmark mb-6">
-            <div>WEDNESDAY THE 14th AUGUST 7.15pm</div>
-            <div class="text-lg text-gray-600">Guest Speaker - The life and times of DUKE ELLINGTON plus HOAGY CARMICHAEL a life in music</div>
-          </li>
-          <li class="checkmark mb-6">
-            <div>WEDNESDAY THE 11TH SEPTEMBER</div>
-            <div class="text-lg text-gray-600">Recorded music presentation 3 members present 40 minutes of recorded jazz music</div>
-          </li>
-        </ul>
-      </div> <!-- end projects -->
-    </div>
     
-    <div class="overflow-x-hidden">
-      <div class="projects container-inner mx-auto text-xl border-t border-gray-500 border-b py-16 mb-16 relative">
-        <h2 class="font-bold mb-6" id="events">Recent Newsletters</h2>
+    
+    
 
-        <div class="absolute right-0" style="top: 50px; transform: translate(100%) rotate(180deg)">
-          <svg width="170px" height="170px"><use xlink:href="#dots-triangle" /></svg>
-        </div>
-
-        <ul class="text-lg sm:text-xl">
-          <li class="checkmark mb-6">
-            <div>August 2019</div>
-            <div class="text-lg text-gray-600"><a href="/downloads/newsletter-august-2019.pdf">Download the August 2019 Newsletter (PDF)</a></div>
-          </li>
-          <li class="checkmark mb-6">
-            <div>July 2019</div>
-            <div class="text-lg text-gray-600"><a href="/downloads/newsletter-july-2019.pdf">Download the July 2019 Newsletter (PDF)</a></div>
-          </li>
-          <li class="checkmark mb-6">
-            <div>June 2019</div>
-            <div class="text-lg text-gray-600"><a href="/downloads/newsletter-june-2019.pdf">Download the June 2019 Newsletter (PDF)</a></div>
-          </li>
-          <li class="checkmark mb-6">
-            <div>May 2019</div>
-            <div class="text-lg text-gray-600"><a href="/downloads/newsletter-may-2019.pdf">Download the May 2019 Newsletter (PDF)</a></div>
-          </li>
-          <li class="checkmark mb-6">
-            <div>April 2019</div>
-            <div class="text-lg text-gray-600"><a href="/downloads/newsletter-april-2019.pdf">Download the April 2019 Newsletter (PDF)</a></div>
-          </li>
-          <li class="checkmark mb-6">
-            <div>March 2019</div>
-            <div class="text-lg text-gray-600"><a href="/downloads/newsletter-march-2019.pdf">Download the March 2019 Newsletter (PDF)</a></div>
-          </li>
-          <li class="checkmark mb-6">
-            <div>February 2019</div>
-            <div class="text-lg text-gray-600"><a href="/downloads/newsletter-february-2019.pdf">Download the February 2019 Newsletter (PDF)</a></div>
-          </li>
-          <li class="checkmark mb-6">
-            <div>January 2019</div>
-            <div class="text-lg text-gray-600"><a href="/downloads/newsletter-january-2019.pdf">Download the January 2019 Newsletter (PDF)</a></div>
-          </li>         
-          <li class="checkmark mb-6">
-            <div>All other newsletters and more</div>
-            <div class="text-lg text-gray-600"><a href="/blog">Visit our Blog section!</a></div>
-          </li>
-        </ul>
-      </div> <!-- end projects -->
-    </div>
-
-    <!--<div class="overflow-x-hidden border-gray-200 border-b">
-      <div class="get-to-know-me container-inner mx-auto text-xl pb-16 relative">
-        <h2 class="font-bold mb-6" id="about">Get to know me:</h2>
-
-        <div class="absolute left-0" style="top: 50px; transform: translateX(-100%)">
-          <svg width="170px" height="170px"><use xlink:href="#dots-triangle" /></svg>
-        </div>
-
-        <div class="flex flex-col sm:flex-row justify-between items-center mb-16">
-          <div><g-image src="../../static/avatar.jpg" alt="avatar" class="w-32 h-32 rounded-full mb-8 lg:mb-0" /></div>
-          <div class="flex-1 text-lg sm:text-xl ml-6">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eum quae architecto minus molestias iusto ducimus fuga quo quia libero dignissimos, fugiat modi assumenda debitis laboriosam.</div>
-        </div>
-
-        <div class="responsive-container">
-          <iframe class="responsive-iframe w-full h-full top-0 left-0 border-0" src="https://www.youtube.com/embed/uHo6o1TNQeE" style="border:0;" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-        </div>
-      </div>--> <!-- end get-to-know me -->
+   
     </div>
 
     <div class="overflow-x-hidden">
       <div class="contact-me bg-background-secondary pt-16">
         <div class="container-inner mx-auto text-xl pb-4 relative">
-          <h2 class="font-bold mb-6" id="contact">Contact us:</h2>
+          <h2 class="font-bold mb-6" id="contact">RSVP:</h2>
 
           <div class="absolute right-0 top-0" style="transform: translate(100%) rotate(180deg)">
             <svg width="170px" height="170px"><use xlink:href="#dots-triangle" /></svg>
@@ -152,7 +56,7 @@
                           Name
                       </label>
 
-                      <input type="text" name="name" id="name" placeholder="Joe Bloggs" class="block w-full bg-background-form border border-border-color-primary shadow rounded outline-none focus:border-green-700 mb-2 p-4" 
+                      <input type="text" name="name" id="name" placeholder="Your Full Name" class="block w-full bg-background-form border border-border-color-primary shadow rounded outline-none focus:border-green-700 mb-2 p-4" 
                       v-model="formData.name"
                       required>
                   </div>
@@ -173,8 +77,28 @@
                       Message
                   </label>
 
-                  <textarea id="message" rows="5" name="message" class="block w-full bg-background-form border border-border-color-primary shadow rounded outline-none appearance-none focus:border-orange-700 mb-2 px-4 py-4" placeholder="Enter your message here." 
+                  <textarea id="message" rows="5" name="message" class="block w-full bg-background-form border border-border-color-primary shadow rounded outline-none appearance-none focus:border-orange-700 mb-2 px-4 py-4" placeholder="Any special messages you wish to send us..." 
                   v-model="formData.message"
+                  required></textarea>
+              </div>
+
+              <div class="w-full mb-12">
+                  <label class="block text-copy-primary mb-2" for="songs">
+                      Song Choices
+                  </label>
+
+                  <textarea id="songs" rows="5" name="songs" class="block w-full bg-background-form border border-border-color-primary shadow rounded outline-none appearance-none focus:border-orange-700 mb-2 px-4 py-4" placeholder="Enter your song choices here for our DJ" 
+                  v-model="formData.songs"
+                  required></textarea>
+              </div>
+
+              <div class="w-full mb-12">
+                  <label class="block text-copy-primary mb-2" for="dietary">
+                      Dietary Requirements
+                  </label>
+
+                  <textarea id="dietary" rows="5" name="dietary" class="block w-full bg-background-form border border-border-color-primary shadow rounded outline-none appearance-none focus:border-orange-700 mb-2 px-4 py-4" placeholder="Please let us know if you have an special dietary requirements." 
+                  v-model="formData.dietary"
                   required></textarea>
               </div>
 
