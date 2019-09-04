fce677f05c7e0d63476d58106ca7dc00875158a1
Nick Lewis <nickeblewis@gmail.com>
2019-09-04 22:08:44 +0100

    fixes

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -8,25 +8,6 @@
 
     </div> <!-- end hero -->
 
-    <div class="container-inner mx-auto" id="about">
-      <p class="text-lg sm:text-xl mb-4">Please RSVP etc etc</p>
-      
-      <p class="text-lg sm:text-xl mb-4">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Excepturi optio odit mollitia illo ullam rem qui, nulla sit vero asperiores magni, quidem, impedit dolores debitis dignissimos molestiae commodi dicta eligendi!</p>
-
-      <p class="text-lg sm:text-xl mb-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus voluptates quam aliquam atque voluptatum, impedit, temporibus consequatur, quae omnis earum autem provident nisi explicabo eveniet neque quod numquam voluptate illo.</p>
-        
-
-
-    
-    </div>
-
-    
-    
-    
-
-   
-    </div>
-
     <div class="overflow-x-hidden">
       <div class="contact-me bg-background-secondary pt-16">
         <div class="container-inner mx-auto text-xl pb-4 relative">
@@ -85,22 +66,14 @@
                   </div>
               </div>
 
-              <div class="w-full mb-12">
-                  <label class="block text-copy-primary mb-2" for="message">
-                      Message
-                  </label>
-
-                  <textarea id="message" rows="5" name="message" class="block w-full bg-background-form border border-border-color-primary shadow rounded outline-none appearance-none focus:border-orange-700 mb-2 px-4 py-4" placeholder="Any special messages you wish to send us..." 
-                  v-model="formData.message"
-                  required></textarea>
-              </div>
+              
 
               <div class="w-full mb-12">
                   <label class="block text-copy-primary mb-2" for="songs">
                       Song Choices
                   </label>
 
-                  <textarea id="songs" rows="5" name="songs" class="block w-full bg-background-form border border-border-color-primary shadow rounded outline-none appearance-none focus:border-orange-700 mb-2 px-4 py-4" placeholder="Enter your song choices here for our DJ" 
+                  <textarea id="songs" rows="5" name="songs" class="block w-full bg-background-form border border-border-color-primary shadow rounded outline-none appearance-none focus:border-orange-700 mb-2 px-4 py-4" placeholder="Please let us know your favourite song, what would you love the DJ to play on the night?" 
                   v-model="formData.songs"
                   required></textarea>
               </div>
@@ -110,11 +83,20 @@
                       Dietary Requirements
                   </label>
 
-                  <textarea id="dietary" rows="5" name="dietary" class="block w-full bg-background-form border border-border-color-primary shadow rounded outline-none appearance-none focus:border-orange-700 mb-2 px-4 py-4" placeholder="Please let us know if you have an special dietary requirements." 
+                  <textarea id="dietary" rows="5" name="dietary" class="block w-full bg-background-form border border-border-color-primary shadow rounded outline-none appearance-none focus:border-orange-700 mb-2 px-4 py-4" placeholder="Please let us know if you have any special dietary requirements." 
                   v-model="formData.dietary"
                   required></textarea>
               </div>
 
+              <div class="w-full mb-12">
+                  <label class="block text-copy-primary mb-2" for="message">
+                      Extra information
+                  </label>
+
+                  <textarea id="message" rows="5" name="message" class="block w-full bg-background-form border border-border-color-primary shadow rounded outline-none appearance-none focus:border-orange-700 mb-2 px-4 py-4" placeholder="Please let us know the names of your own guests, such as your own children etc" 
+                  v-model="formData.message"
+                  required></textarea>
+              </div>
               <div class="flex justify-end w-full">
                   <button type="submit" value="submit" class="block bg-orange-700 hover:bg-orange-800 text-white text-sm font-semibold tracking-wide uppercase shadow rounded cursor-pointer px-6 py-3">Submit</button>
               </div>
